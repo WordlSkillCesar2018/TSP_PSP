a37e3d1f49298988854850f413bad21080af2d8d
WordlSkillCesar2018 <wskcesar2018@gmail.com>
2018-09-01 13:19:50 -0500

    |

--- app/src/main/java/com/example/worldskills/tsp_psp/Proyectplansumary.java
+++ app/src/main/java/com/example/worldskills/tsp_psp/Proyectplansumary.java
@@ -1,17 +1,26 @@
 package com.example.worldskills.tsp_psp;
 
 import android.app.FragmentManager;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Proyectplansumary extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proyectplansumary);
+        Adaptador adaptador= new Adaptador(getSupportFragmentManager());
+        viewPager = findViewById(R.id.vp);
+        viewPager.setAdapter(adaptador);
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
 
@@ -23,13 +32,40 @@ class Adaptador extends FragmentStatePagerAdapter{
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Fragment fragment;
+        switch (position){
+            case 0:
+                fragment = new Fragment1();
+                break;
+            case 1:
+                fragment = new Fragment2();
+                break;
+            case 2:
+                fragment = new Fragment3();
+                break;
+
+                default:return null;
+
+        }
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 3;
     }
 
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "TimeLog";
+            case 1:
+               return "DefectLog";
+            case 2:
+                return "Proyect Plan Sumary";
 
+        }
+        return null;
+    }
 }
--- app/src/main/java/com/example/worldskills/tsp_psp/Time_log.java
+++ app/src/main/java/com/example/worldskills/tsp_psp/Time_log.java
@@ -1,38 +1,53 @@
 package com.example.worldskills.tsp_psp;
 
 import android.annotation.SuppressLint;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Time_log extends AppCompatActivity {
 Spinner spinner;
-TextView hinicio;
+TextView hinicio,hfin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_log);
         hinicio=findViewById(R.id.hinicio);
         spinner=findViewById(R.id.spin);
+        hfin = findViewById(R.id.textView7);
         String[] fase=new String[]{"PLAN","DLD","CODE","COMPILE","UT","PM"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fase);
         spinner.setAdapter(adapter);
     }
 
     public void inicio(View view) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat fecha=new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss z");
-        String a= String.valueOf(fecha);
-        hinicio.setText((CharSequence) a);
-
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("h:mm");
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd-MM-yyyy");
+        String a = simpleDateFormat1.format(date)+" "+simpleDateFormat.format(date);
+        hinicio.setText(a);
     }
 
     public void parar(View view) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat fecha=new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss z");
-        hinicio.setText((CharSequence) fecha);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("h:mm");
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd-MM-yyyy");
+        String a = simpleDateFormat1.format(date)+" "+simpleDateFormat.format(date);
+        hfin.setText(a);
+    }
+
+    public void registro(View view) {
+        Developer developer = new Developer(getApplicationContext());
+        SQLiteDatabase sqLiteDatabase = developer.getWritableDatabase();
+        //sqLiteDatabase.execSQL("insert into timeLog values ('"++"','"++"','"++"','"++"','"++"',) ");
+        Toast.makeText(getApplicationContext(),"Timelog Guardado", Toast.LENGTH_LONG).show();
     }
 }
