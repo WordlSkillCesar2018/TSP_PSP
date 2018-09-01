1d0158624223843da75c31be9978c6dceb4d526a
WordlSkillCesar2018 <wskcesar2018@gmail.com>
2018-09-01 13:22:29 -0500

    |

---
+++ app/src/main/java/com/example/worldskills/tsp_psp/Fragment1.java
@@ -0,0 +1,19 @@
+package com.example.worldskills.tsp_psp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("ValidFragment")
+class Fragment1 extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = getLayoutInflater().inflate(R.layout.fragment1,null,false);
+
+        return view;
+    }
+}
